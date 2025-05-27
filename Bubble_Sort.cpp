75c23027b71fe726da26400ab945b9b06c8ba1f7
Bastian Bernal <bbernal2022@udec.cl>
2025-05-27 10:26:29 -0400

    Feat: Se agrega Selection y Bubble Sort.

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include "utils.hpp"
+
+using namespace std;
+
+
+// Versión optimizada del algoritmo Bubble Sort
+void bubbleSortOptimized(vector<int>& arr) {
+    int n = arr.size();
+    bool swapped;
+
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        // Si no hubo intercambio, el arreglo ya está ordenado
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+int main() {
+    // Leer el archivo de entrada
+    cout << "Ingrese el nombre del archivo de entrada: ";
+    string archivo;
+    cin >> archivo;
+    vector<int> data = readFile(archivo);
+
+    // Iniciar el temporizador
+    startTimer();
+
+    // Ordenar el vector usando Insertion Sort
+    bubbleSortOptimized(data);
+
+    // Detener el temporizador
+    stopTimer();
+
+    // Verificar si el vector está ordenado
+    if (!isSorted(data)) {
+        cout << "Error: El vector no está ordenado correctamente." << endl;
+    }
+    
+    return 0;
+}
---
+++ Selection_Sort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include "utils.hpp"
+
+using namespace std;
+
+void selectionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+
+        // Encontrar el índice del elemento mínimo en el subarreglo no ordenado
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Intercambiar el elemento mínimo encontrado con el primer elemento del subarreglo no ordenado
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main() {
+    // Leer el archivo de entrada
+    cout << "Ingrese el nombre del archivo de entrada: ";
+    string archivo;
+    cin >> archivo;
+    vector<int> data = readFile(archivo);
+
+    // Iniciar el temporizador
+    startTimer();
+
+    // Ordenar el vector usando Insertion Sort
+    selectionSort(data);
+
+    // Detener el temporizador
+    stopTimer();
+
+    // Verificar si el vector está ordenado
+    if (!isSorted(data)) {
+        cout << "Error: El vector no está ordenado correctamente." << endl;
+    }
+    
+    return 0;
+}
